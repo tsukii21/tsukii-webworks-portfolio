29f524976eae9736ec638660623eff1dd0f4ed5a
Jaidev Das <jaidevkr2@gmail.com>
2020-12-10 20:56:23 +0530

    finished basic site

---
+++ public/js/scripts.js
@@ -0,0 +1,18 @@
+const s = skrollr.init();
+new WOW().init();
+const root = document.querySelector(":root");
+const hoverables = document.querySelectorAll("a");
+document.addEventListener("mousemove", (e) => {
+  root.style.setProperty("--x", e.clientX + "px");
+  root.style.setProperty("--y", e.clientY + "px");
+});
+for (let i = 0; i < hoverables.length; i++) {
+  hoverables[i].addEventListener("mouseenter", onMouseHover);
+  hoverables[i].addEventListener("mouseleave", onMouseHoverOut);
+}
+function onMouseHover() {
+  root.style.setProperty("--cSize", "90px");
+}
+function onMouseHoverOut() {
+  root.style.setProperty("--cSize", "34px");
+}
